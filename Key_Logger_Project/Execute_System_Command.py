1e9d07995f7ee6be59bc589ae0afad6bedb18967
Jason-Mez <46197717+Jason-Mez@users.noreply.github.com>
2021-04-06 10:39:21 +0800

    Delete Execute_System_Command.py

--- Key_Logger_Project/Execute_System_Command.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-#This scipt will focus on executing system commands on Windows.
-
-import subprocess
-#Command will be stored in here.
-command = "dir"
-
-subprocess.Popen(command, shell = True)
