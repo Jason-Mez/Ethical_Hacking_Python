51d9b0167460ead35df80590c2dffd2d03b94526
Jason-Mez <46197717+Jason-Mez@users.noreply.github.com>
2021-04-22 14:34:13 +0800

    Update Implementing_A_Server.py

--- Backdoor_Proj/Implementing_A_Server.py
+++ Backdoor_Proj/Implementing_A_Server.py
@@ -1,1 +1,12 @@
+#!/usr/bin/python 
+import socket
 
+listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+
+#Listen to incoming connections. 
+listener.bind("Please put the IP address you want to bind to", "<select the port number you want>" ) 
+listener.listen(0)  
+print("Waiting for incoming connections. ")
+listener.accept() 
+print("We have a connection.")
